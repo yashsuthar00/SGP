50d57183a5de91a4ab9ccf6e4ea4bd6f4673fe25
Yash Suthar <yashsuthar0309@gmail.com>
2024-09-07 01:38:50 +0530

    search using studentId

--- public/assets/js/searchbar.js
+++ public/assets/js/searchbar.js
@@ -1,77 +1,79 @@
 async function performAutocomplete() {
-    const query = document.getElementById('searchBar').value;
-    if (query.length === 0) {
-      document.getElementById('suggestions').innerHTML = '';
-      return;
-    }
-    try {
-      const response = await fetch(`/api/student-logs?q=${encodeURIComponent(query)}`);
-      const users = await response.json();
-      displaySuggestions(users);
-    } catch (err) {
-      console.error('Error fetching autocomplete results:', err);
-    }
+  const query = document.getElementById("searchBar").value;
+  if (query.length === 0) {
+    document.getElementById("suggestions").innerHTML = "";
+    return;
   }
+  try {
+    const response = await fetch(
+      `/api/student-logs?q=${encodeURIComponent(query)}`,
+    );
+    const users = await response.json();
+    displaySuggestions(users);
+  } catch (err) {
+    console.error("Error fetching autocomplete results:", err);
+  }
+}
 
-  document.getElementById('searchBar').addEventListener('keypress', function(event) {
-  if (event.key === 'Enter') {
-    event.preventDefault(); 
-    const query = event.target.value;
-    if (query) {
-      document.getElementById('suggestions').innerHTML = ' ';
-      SearchById();
+document
+  .getElementById("searchBar")
+  .addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      const query = event.target.value;
+      if (query) {
+        document.getElementById("suggestions").innerHTML = " ";
+        SearchById();
+      }
     }
-  }
-});
-  
-  function displaySuggestions(users) {
-    const suggestionsDiv = document.getElementById('suggestions');
-    suggestionsDiv.innerHTML = ''; 
-    if (users.length === 0) {
-      suggestionsDiv.innerHTML = 'No suggestions available.';
-    } else {
-      users.forEach(user => {
-        const suggestionItem = document.createElement('div');
-        suggestionItem.className = 'suggestion-item';
-        suggestionItem.textContent = user.Fname;
-        suggestionItem.onclick = () => selectSuggestion(user.Fname);
-        // suggestionItem.onclick = () => SearchById();
-        suggestionsDiv.appendChild(suggestionItem);
-      });
-    }
-  }
-  
-  function selectSuggestion(name) {
-    document.getElementById('searchBar').value = name;
-    document.getElementById('suggestions').innerHTML = '';
-    SearchById();
-  }
-  
+  });
 
-  function SearchById() {
-            const studentID = document.getElementById('searchBar').value;
-            
-            axios.get(`/api/student-logs/${studentID}`)
-            .then(response => {
-                if (response.data == null) {
-                    document.getElementById('studentData').reset();
-                } 
-                else {
-                    const data = response.data;
+function displaySuggestions(users) {
+  const suggestionsDiv = document.getElementById("suggestions");
+  suggestionsDiv.innerHTML = "";
+  if (users.length === 0) {
+    suggestionsDiv.innerHTML = "No suggestions available.";
+  } else {
+    users.forEach((user) => {
+      const suggestionItem = document.createElement("div");
+      suggestionItem.className = "suggestion-item";
+      suggestionItem.textContent = user.studentId;
+      suggestionItem.onclick = () => selectSuggestion(user.studentId);
+      // suggestionItem.onclick = () => SearchById();
+      suggestionsDiv.appendChild(suggestionItem);
+    });
+  }
+}
 
-                    document.getElementById('firstName').value = data.Fname;
-                    document.getElementById('lastName').value = data.Lname;
-                    document.getElementById('dob').value = data.DOB;
-                    document.getElementById('email').value = data.Email;
-                    document.getElementById('contact').value = data.Contact;
-                    document.getElementById('address').value = data.Address;
-                    document.getElementById('admission_date').value = data.AdmissionDate;
-                    document.getElementById('course').value = data.Course;
-                    document.getElementById('Sem').value = data.Semester;
-                }
-                
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-            });
-        }
+function selectSuggestion(name) {
+  document.getElementById("searchBar").value = name;
+  document.getElementById("suggestions").innerHTML = "";
+  SearchById();
+}
+
+function SearchById() {
+  const studentID = document.getElementById("searchBar").value;
+
+  axios
+    .get(`/api/student-logs/${studentID}`)
+    .then((response) => {
+      if (response.data == null) {
+        document.getElementById("studentData").reset();
+      } else {
+        const data = response.data;
+
+        document.getElementById("firstName").value = data.Fname;
+        document.getElementById("lastName").value = data.Lname;
+        document.getElementById("dob").value = data.DOB;
+        document.getElementById("email").value = data.Email;
+        document.getElementById("contact").value = data.Contact;
+        document.getElementById("address").value = data.Address;
+        document.getElementById("admission_date").value = data.AdmissionDate;
+        document.getElementById("course").value = data.Course;
+        document.getElementById("Sem").value = data.Semester;
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+}
